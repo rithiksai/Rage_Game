1493e61077c823c233280f4f66c75e6478f70e59
Rithik <rithikmotupalli@gmail.com>
2020-08-19 12:26:00 +0530

    2nd upload
    added the blob 3D designed just for now (we will change it later)

---
+++ RageGame/Assets/Scripts/Blob.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blob : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float acc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //test jump
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector3(0f, acc * rb.mass, 0f), ForceMode.Impulse);
+        }
+    }
+}
