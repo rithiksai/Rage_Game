c18450356ae337e5058962a5958d54b8e1456928
Rithik <rithikmotupalli@gmail.com>
2020-08-19 15:04:52 +0530

    Added movement
    added movement to the blob based on the user drag of the mouse

--- RageGame/Assets/Scripts/Blob.cs
+++ RageGame/Assets/Scripts/Blob.cs
@@ -7,6 +7,9 @@ public class Blob : MonoBehaviour
     private Rigidbody rb;
     public float acc;
 
+    private Vector2 spoint, epoint, offset;
+    private Vector3 jumpVect;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,5 +24,23 @@ public class Blob : MonoBehaviour
         {
             rb.AddForce(new Vector3(0f, acc * rb.mass, 0f), ForceMode.Impulse);
         }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            spoint = Input.mousePosition;
+            //Debug.Log(spoint);
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            epoint = Input.mousePosition;
+
+            offset = spoint - epoint;
+
+            jumpVect = new Vector3(offset.x, 100f * acc, offset.y);
+
+            rb.AddForce(jumpVect * .1f, ForceMode.Impulse);
+        }
+
+
     }
 }
