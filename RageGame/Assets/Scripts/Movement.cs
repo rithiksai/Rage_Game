1ba9bfb2e4225a25344eac5765062494e107c45e
akrem420 <69857988+akrem420@users.noreply.github.com>
2020-08-19 06:30:36 -0700

    k

---
+++ RageGame/Assets/Scripts/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Touch drag;
+    public float dragSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dragSpeed = 0.1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            drag = Input.GetTouch(0);
+            if(drag.phase == TouchPhase.Moved)
+            {
+                transform.position = new Vector3(
+                    transform.position.x + drag.deltaPosition.x * dragSpeed,
+                    transform.position.y,
+                    transform.position.z + drag.deltaPosition.y * dragSpeed
+                    );
+
+
+            }
+        }
+    }
+}
