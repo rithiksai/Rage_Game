4ddef1b97bf594e967b9569fa5e1732ba656436e
Fortunate <theframedictator@gmail.com>
2020-08-22 15:40:15 +0200

    Biggest Update Yet
    Dead barrier and some small things

---
+++ RageGame/Assets/Scripts/youded.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class youded : MonoBehaviour
+{
+
+
+
+    
+    // OnCollisionEnter2D is called when this collider2D/rigidbody2D has begun touching another rigidbody2D/collider2D (2D physics only)
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "dead")
+            SceneManager.LoadScene("Map 1");
+    }
+}
