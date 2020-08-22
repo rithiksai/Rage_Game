e0eef2fa6f403e6a9142c7ac2d5bb79811943ce7
SpudSupreme <69838290+SpudSupreme@users.noreply.github.com>
2020-08-22 15:40:56 -0700

    Merge branch 'master' of https://github.com/rithiksai/Rage_Game

---
+++ RageGame/Assets/PauseMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject Pausemenu, PauseButton;
+
+    public void Pause()
+    {
+        Pausemenu.SetActive (true);
+        PauseButton.SetActive (false);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume ()
+    {
+        Pausemenu.SetActive (false);
+        PauseButton.SetActive (true);
+        Time.timeScale = 1f;
+    }
+    public void Mainmenu ()
+    {
+        SceneManager.LoadScene (0);
+    }
+}
---
+++ RageGame/Assets/Scripts/MovePlatform.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class MovePlatform : MonoBehaviour
+{
+    //Hope the script works 
+
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f;
+
+    float movementFactor; 
+    Vector3 startingPos;
+
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    void Update()
+    {
+        float cycles = Time.time / period; 
+
+        const float tau = Mathf.PI * 2f; // about 6.28
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = rawSinWave / 2f + 0.5f;
+        Vector3 offset = movementFactor * movementVector;
+        transform.position = startingPos + offset;
+    }
+}
