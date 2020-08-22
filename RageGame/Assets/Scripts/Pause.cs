0035b829be09f68b55b4567a165a16056b1f7123
akrem420 <69857988+akrem420@users.noreply.github.com>
2020-08-22 08:35:42 -0700

    Pause menu

---
+++ RageGame/Assets/Scripts/Pause.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+ 
+        public GameObject PauseMenu, PauseButton;
+
+        public void pause()
+        {
+            PauseMenu.SetActive(true);
+            PauseButton.SetActive(false);
+            Time.timeScale = 0f;
+        }
+
+        public void Resume()
+        {
+            PauseMenu.SetActive(false);
+            PauseButton.SetActive(true);
+            Time.timeScale = 1f;
+        }
+        public void Mainmenu()
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
