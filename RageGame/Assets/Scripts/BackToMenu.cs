de13467e2acf5634c711302a9c05e0647adfab28
Fortunate <theframedictator@gmail.com>
2020-08-30 15:54:48 +0000

    map

---
+++ RageGame/Assets/Scripts/BackToMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Moederskut()
+    {
+        SceneManager.LoadScene("menu");
+    }
+
+    // Update is called once per frame
+    public void OnClick()
+    {
+        Debug.Log("CLick");
+
+    }
+}
