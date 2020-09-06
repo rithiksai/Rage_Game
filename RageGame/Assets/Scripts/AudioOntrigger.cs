9bd57d70bec9b9d79e5252a3695f8409d837b84c
Fortunate <theframedictator@gmail.com>
2020-09-06 10:28:38 +0200

    New push

---
+++ RageGame/Assets/Scripts/AudioOntrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioOntrigger : MonoBehaviour
+{
+    public AudioClip VoiceOverClip;
+    public AudioSource VoiceOverThing;
+    bool soundHasPlayed = false;
+
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        if (!soundHasPlayed)
+        {
+            GetComponent<AudioSource>().PlayOneShot(VoiceOverClip);
+
+            VoiceOverThing.Play(); soundHasPlayed = true;
+
+
+
+
+
+
+        }
+    }
+     
+
+    // Update is called once per frame
+
+}
